73f3f80c29944e7097808a8184ebd531ecf66158
Daniels Balika <danikbalik@gmail.com>
2025-03-12 21:55:08 +0200

    Update: styling with tailwindcss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import '../styles/Card.css'
 
-function Card({ id, name, description, price }) {
+function Card({ id, name, description, price, additional_price}) {
   return (
-    <li key={id} className="subscription-item">
-      <h3>{name}</h3>
-      {description && <p>{description}</p>}
-      {price && <p className="price">Cena: {price} €</p>}
-      <button className="choose-button">Izveleties</button>
+    <li key={id} className="card-item">
+      <div className="max-w-sm bg-white border border-gray-100 rounded-t-lg shadow-t-lg ">
+        {name && <h3 className='mb-2 text-2xl font-bold text-gray-900 text-center'>{name}</h3>}
+        {description && <p className='mb-3 font-normal text-gray-700'>{description}</p>}
+    </div>
+    <div className='bg-gray-200 rounded-b-lg'>
+        {price && <p className='pt-3 pb-3 font-normal text-gray-700 text-center'>Cena: {price}</p>}
+        {additional_price && <p className='pb-3 font-normal text-gray-700 text-center'>Par katru nakamo stundu: {additional_price} eiro.</p>}
+        <div class="flex justify-center items-center">
+            <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3'>Izveleties</button>      
+        </div>
+       
+    </div>
+        
+      
     </li>
   );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,3 +1,4 @@
+
 import { useState, useEffect } from 'react';
 import { supabase } from '../utils/supabase';
 
@@ -18,12 +19,12 @@ function MainPage() {
             .select('*'); 
   
           if (error) {
-            console.error('Ошибка при получении подписок:', error);
+            console.error('Notika kluda:', error);
           } else {
             setSubscriptions(data);
           }
         } catch (err) {
-          console.error('Ошибка:', err);
+          console.error('Kluda:', err);
         }
       };
   
@@ -31,34 +32,35 @@ function MainPage() {
     }, []);
   
     return (
-    <div className="full">
-      <div className="main-page">
-        <header className="header">
-          <h1>MOOMENTUM</h1>
+        <div className="min-h-screen flex flex-col bg-stone-100 text-white">
+
+        <header className="py-4 bg-sky-800 text-center">
+          <h1 className="text-3xl font-bold">MOOMENTUM</h1>
         </header>
-  
-        <main className="content">
-          <h2>Izvele</h2>
+      
+        <main className="flex-grow py-8">
+          <h2 className="text-gray-950 text-2xl text-center mb-4">Izvele</h2>
           {subscriptions.length === 0 ? (
-            <p>Notiek ielade</p>
+            <p className="text-center">Notiek ielade</p>
           ) : (
-            <ul className="subscriptions-list">
-              {subscriptions.map((sub) => (
-                <Card 
-                  key={sub.id}
-                  id={sub.id}
-                  name={sub.name}
-                  description={sub.description}
-                  price={sub.price}
-                />
-              ))}
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto px-4 justify-items-center">
+                {subscriptions.map((sub) => (
+                    <Card 
+                    key={sub.id}
+                    id={sub.id}
+                    name={sub.name}
+                    description={sub.description}
+                    price={sub.price}
+                    additional_price={sub.additional_hour_price}
+                    />
+                ))}
             </ul>
           )}
         </main>
-        <footer className="footer">
+      
+        <footer className="py-4 bg-sky-950 text-center">
           <p>© MOOMENTUM</p>
         </footer>
-      </div>
       </div>
     );
   }
